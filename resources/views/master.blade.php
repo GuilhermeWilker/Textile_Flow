46bd4fc276c2b1964600db0088f1e354d38a28bc
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-05-28 19:31:22 -0300

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Textile Flow</title>
+    <link rel="stylesheet" href="/css/app.css" />
+</head>
+
+<body>
+
+    <main class="container">
+        <header class="header">
+            <div class="img"></div>
+            <h3>Calina confecções</h3>
+        </header>
+
+        <section class="actions_container">
+
+            <div class="action_box">
+                <h2>12:53:25seg</h2>
+            </div>
+
+            <div class="action_box">
+                <h2>Gerar relatórios</h2>
+            </div>
+
+            <div class="action_box">
+                <h2>Checar ganhos e despesas</h2>
+            </div>
+
+        </section>
+
+        <section class="stock_display">
+
+            <header class="stock_header">
+                <h2 class="title">Estoque de itens</h2>
+                <a href="">Adicionar item</a>
+            </header>
+
+
+            <article class="actions_container">
+                <div class="action_box">
+                    <div class="img_box"></div>
+                    <p>Fios vermelhos</p>
+                </div>
+
+                <div class="action_box">
+                    <div class="img_box"></div>
+                    <p>Fios Amarelos</p>
+                </div>
+
+                <div class="action_box">
+                    <div class="img_box"></div>
+                    <p>Fios vermelhos</p>
+                </div>
+            </article>
+
+
+        </section>
+
+        <div class="navbar">
+            aaaa
+        </div>
+    </main>
+</body>
+
+</html>
